5b37bab3bc9adaa39cce66b96b9c1347cf39c265
Milos Simic <milos.panj@gmail.com>
2018-04-13 16:01:17 +0200

    URI edit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:KiselaWater2345@localhost/airdropdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:KiselaWater2345@localhost/airdropdb'
 db = SQLAlchemy(app)
 
 class Dropovi(db.Model):
