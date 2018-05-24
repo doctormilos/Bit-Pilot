13bd650c642d7611cf27ab8a54346937d24ae25e
Milos Simic <milos.panj@gmail.com>
2018-05-24 18:15:18 +0200

    mzoda
    ms

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ class User(db.Model):
 @app.route('/')
 def svidropovi():
 	svidropovi = Dropovi.query.filter_by(active=True)
-	if request.headers['HTTP_CF_IPCOUNTRY'] == 'RS':
+	if request.headers['CF_IPCOUNTRY'] == 'RS':
 		return redirect(url_for('pocetna'))
 	else:
 		return render_template('index.html', svidropovi=svidropovi)
